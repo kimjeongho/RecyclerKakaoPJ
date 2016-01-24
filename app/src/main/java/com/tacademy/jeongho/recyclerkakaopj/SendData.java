16f4a083f40c4f0289f332e178cd3676b9d8f1de
kimjeongho <jeongho9496@daum.net>
2016-01-24 22:09:35 +0900

    update ver.2.0

--- app/src/main/java/com/tacademy/jeongho/recyclerkakaopj/SendData.java
+++ app/src/main/java/com/tacademy/jeongho/recyclerkakaopj/SendData.java
@@ -6,11 +6,7 @@ import android.graphics.drawable.Drawable;
  * Created by Tacademy on 2016-01-23.
  */
 public class SendData implements ChattingData{
-    public static final int TYPE_RECEIVE = 0;
-    public static final int TYPE_SEND = 1;
-    public static final int TYPE_DATE = 2;
 
-    int type;   // data 분류 나누기 위해 필요
     String message; //메시지 data
     Drawable imageIcon;// image data
 
